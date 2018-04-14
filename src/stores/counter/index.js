c791a1245b27a5efe249aaa5800b9665428086b8
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-04-14 10:26:06 +0700

    Remove useless code

--- src/stores/counter/index.js
+++ src/stores/counter/index.js
@@ -2,9 +2,6 @@ import { observable, action, computed } from "mobx"
 
 class Counter {
     @observable value = 0;
-    @computed get getValue() {
-      return this.value;
-    }
     @action add() {
       this.value += 1
     }
